cd8fe564ff77669e0eecadbec59b61d54935c84b
LouisLiu03 <dalion00@gmail.com>
2022-10-01 22:38:11 +1000

    addFooter

---
+++ src/components/footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="footer text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1> Name: Louis </h1>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MyFooter'
+    }
+</script>
+<style>
+    .footer {
+        margin-top: 5rem;
+        background-color: #000;
+        color: #fff;
+    }
+</style>
